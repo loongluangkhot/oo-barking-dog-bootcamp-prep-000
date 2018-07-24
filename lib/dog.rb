8e5eb1c4e6254f1f11246accf64a356ead36a985
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-24 15:13:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,15 @@
 # Your code goes here!
 class Dog
-  attr_accessor :name
-  
   def initialize(name="Fido")
     @name = name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def name
+    @name
   end
   
   def bark
